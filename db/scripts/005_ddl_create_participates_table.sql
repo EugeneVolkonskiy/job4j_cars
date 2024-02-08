6565c190a53266aa7799c8de32c861d0ab30f5c2
EugeneVolkonskiy <3376361@mail.ru>
2024-02-08 21:19:42 +0300

    2. ManyToMany [#331986]

---
+++ db/scripts/005_ddl_create_participates_table.sql
@@ -0,0 +1,7 @@
+CREATE table participates
+(
+id serial primary key,
+user_id int not null references auto_user(id),
+post_id int not null references auto_post(id),
+UNIQUE (user_id, post_id)
+);
--- src/main/java/ru/job4j/cars/model/Post.java
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -28,4 +28,12 @@ public class Post {
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "post_id")
     private List<PriceHistory> priceHistories = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "post_id")}
+    )
+    private List<User> participates = new ArrayList<>();
 }
