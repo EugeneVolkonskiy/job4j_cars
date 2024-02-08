a7360d8beba9af3cb1909ba16374d9f9590ac5e6
EugeneVolkonskiy <3376361@mail.ru>
2024-02-08 18:18:13 +0300

    1. ToMany [#301848]

---
+++ db/scripts/004_ddl_create_price_history_table.sql
@@ -0,0 +1,8 @@
+CREATE table price_history
+(
+id SERIAL PRIMARY KEY,
+before BIGINT not null,
+after BIGINT not null,
+created TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
+post_id int references auto_post(id)
+);
